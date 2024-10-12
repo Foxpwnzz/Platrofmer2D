ea45310e2d19515fd139be243911e969f1f28a71
Foxpwnzz <s.a.golubev90@gmail.com>
2024-10-12 11:09:59 +0300

    Homework 3 HYZ
    Создать перемещаемую бочку

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -6,21 +6,42 @@ using UnityEngine;
 public class Hero : MonoBehaviour
 {
     [SerializeField] private float _speed;
+    [SerializeField] private float _jumpSpeed;
+    [SerializeField] private LayerCheck _groundCheck;
+
+    private Rigidbody2D _rigidbody;
     private Vector2 _direction;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
 
     public void SetDirection(Vector2 direction)
     {
         _direction = direction;
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
-        if (_direction != Vector2.zero)
+        _rigidbody.velocity = new Vector2(_direction.x * _speed, _rigidbody.velocity.y);
+
+        var isJumping = _direction.y > 0;
+        if (isJumping)
         {
-            var delta = _direction * _speed * Time.deltaTime;
-            var newPosition = new Vector3(transform.position.x + delta.x, transform.position.y + delta.y, transform.position.z);
-            transform.position = newPosition;
+            if (IsGrounded())
+            {
+                _rigidbody.AddForce(Vector2.up * _jumpSpeed, ForceMode2D.Impulse);
+            }
+        } else if (_rigidbody.velocity.y > 0)
+        {
+            _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _rigidbody.velocity.y * 0.5f);
         }
+    }
+
+    private bool IsGrounded()
+    {
+        return _groundCheck.IsTouchingLayer;
     }
 
     public void SaySomething()
